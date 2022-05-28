217f08ed555b5700147b860a5e9354ef3a3a6a58
Belinda chanda <btatend1@stu.vistula.edu.pl>
2022-05-28 23:01:09 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) { RectangleArea ra = new RectangleArea();
+        ra.getDate();
+        ra.computeField();
+        ra.fieldDisplay();
+    }
+}
+	// write your code here
+    }
+}
